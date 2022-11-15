fd618f87cecfd0a08785d0946fab83d63c2dbe69
josefmarcc <56867995+josefmarcc@users.noreply.github.com>
2022-11-15 19:51:23 +0100

    Added token, and new endpoints

--- Routes/userRouter.js
+++ Routes/userRouter.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const userController = require("../Controllers/userController");
+const JWTverify = require("../middleware/verifyToken");
 
 const router = express.Router({ mergeParams: true });
 
 router.post("/signup", userController.signUp);
 router.post("/login", userController.login);
+
+const USE_AUTH = !process.env["SKIP_AUTH"];
+
+if (USE_AUTH) {
+  router.use(JWTverify);
+}
+
+// Endpoints below this line require authentication
+
+router.get("/allUsers", userController.getAllUsers);
+router.get("/user", userController.getUser);
+router.delete("/user", userController.deleteUser);
 
 module.exports = router;
