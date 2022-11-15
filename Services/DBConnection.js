1e82434a2069b34587086549bf0b9a5ae5f5bc85
josefmarcc <56867995+josefmarcc@users.noreply.github.com>
2022-11-15 19:07:59 +0100

    Added encryption, cleanup and fixed nodemon

--- Services/DBConnection.js
+++ Services/DBConnection.js
@@ -4,26 +4,23 @@ const connectionString = process.env.DB_URI;
 const dbName = process.env.DB_NAME;
 let mongoClient;
 
-async function connect(callback){
-	await MongoClient.connect(connectionString, (err, client) => {
-		mongoClient = client;
-		callback();
-	});
+async function connect(callback) {
+  await MongoClient.connect(connectionString, (err, client) => {
+    mongoClient = client;
+    callback();
+  });
 }
 
-async function get(){
-	return await mongoClient.db(dbName);
+async function get() {
+  return await mongoClient.db(dbName);
 }
 
-async function close(){
-	await mongoClient.close();
+async function close() {
+  await mongoClient.close();
 }
 
 module.exports = {
-	connect,
-	get,
-	close
+  connect,
+  get,
+  close,
 };
-
-
-
