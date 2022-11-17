dfb56553dff7112cd57dbf322080ac3268332aee
Frederik Dahl <cph-fd76@cphbusiness.dk>
2022-11-17 13:44:50 +0100

    Update jest-mongodb-config.js

--- jest-mongodb-config.js
+++ jest-mongodb-config.js
@@ -3,7 +3,7 @@ const connectionString = process.env.DB_URI;
 module.exports = {
 	mongodbMemoryServerOptions: {
 		binary: {
-			version: '4.0.3',
+			version: '4.4.18',
 			skipMD5: true,
 		},
 		autoStart: false,
