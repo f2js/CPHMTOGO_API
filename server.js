f75c6d334d6450cba1db880b0653840a8c6985b9
Frederik Dahl <cph-fd76@cphbusiness.dk>
2023-01-02 11:07:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,19 +3,13 @@
 const dbConnection = require('./Services/DBConnection')
 
 const app = require('./app')
-const https = require('https')
+
 
 const port = process.env.API_PORT
 
 dbConnection.connect();
 
-if (process.env.NODE_ENV == 'development') {
+
 	app.listen(port, () => {
 		console.log(`App running locally without sslOptions on port ${port}`)
 	})
-} else {
-	https.createServer(sslOptions, app).listen(port, () => {
-		console.log(`App running on port ${port}`)
-	})
-
-}
