7656eb892d815ed24fc71f34ed27b4f0a6279fb8
Frederik Dahl <cph-fd76@cphbusiness.dk>
2023-01-10 10:46:21 +0100

    Updated server.js for frontend testing

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 // Set .env file as the very first thing
+const fs = require("fs");
+require("dotenv").config({ path: "./.env" });
 
 const dbConnection = require('./Services/DBConnection')
 
@@ -8,7 +10,6 @@ const app = require('./app')
 const port = process.env.API_PORT
 
 dbConnection.connect();
-
 
 	app.listen(port, () => {
 		console.log(`App running locally without sslOptions on port ${port}`)
